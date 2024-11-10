4a5bbbdad84c2462adb5e4c667f7875640c75440
AleAymo <alejandro.aymo2@gmail.com>
2024-11-10 15:56:36 -0300

    ejercicio 3

---
+++ Tercera Entrega/index.js
@@ -0,0 +1,64 @@
+const pizzas = [
+  {
+    id: 1,
+    nombre: "pizza de Muzzarella",
+    precio: 500,
+    ingredientes: ["Muzzarella", "Tomate", "Aceitunas"],
+    imagen: "./img/muzzarella.png",
+  },
+
+  {
+    id: 2,
+    nombre: "pizza de Cebolla",
+    precio: 1500,
+    ingredientes: ["Muzzarella", "Tomate", "Cebolla"],
+    imagen: "./img/cebolla.png",
+  },
+
+  {
+    id: 3,
+    nombre: "pizza 4 Quesos",
+    precio: 1380,
+    ingredientes: [
+      "Muzzarella",
+      "Tomate",
+      "Queso Azul",
+      "Parmesano",
+      "Roquefort",
+    ],
+    imagen: "./img/4quesos.png",
+  },
+
+  {
+    id: 4,
+    nombre: "pizza Especial",
+    precio: 1000,
+    ingredientes: ["Muzzarella", "Tomate", "Rucula", "Jamón"],
+    imagen: "./img/especial.png",
+  },
+
+  {
+    id: 5,
+    nombre: "pizza con Anana",
+    precio: 600,
+    ingredientes: ["Muzzarella", "Tomate", "Anana"],
+    imagen: "./img/anana.png",
+  },
+];
+
+
+document.getElementById('search-btn').addEventListener('click', function() {
+  const number = document.getElementById('number-input').Value;
+  const resultcontainer = document.getElementById('result-container');
+  const resulttext = document.getElementById('result');
+})
+
+if (number) {
+  const result = number * 2;
+
+  resulttext.textcontent = `El resultado de multiplicar ${number} por 2 es: ${result}`;
+  resultcontainer.style.display = 'block';
+} else {
+  resultcontainer.style.display = 'none';
+  alert('Por favor ingrese un número.');
+}
